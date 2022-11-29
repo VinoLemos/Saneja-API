300ebccfd14cc978a362d1b999b60ee8aa68c005
Sabrina Andrade <56515117+sabrinandrde@users.noreply.github.com>
2022-11-29 11:32:10 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -45,11 +45,11 @@ builder.Services.AddSingleton(mapper);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
