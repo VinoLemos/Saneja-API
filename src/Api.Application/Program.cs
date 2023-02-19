d7cb70a78c9e51aaf24e75c2182933ef1356cbbd
Vinicius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2023-02-19 14:12:24 -0300

    Created sql connection string and builder configurations

--- src/Api.Application/Program.cs
+++ src/Api.Application/Program.cs
@@ -1,11 +1,19 @@
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MyContext>(options => 
+        options.UseMySql(mySqlConnection,
+            ServerVersion.AutoDetect(mySqlConnection)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
