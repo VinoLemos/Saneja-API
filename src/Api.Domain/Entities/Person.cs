5f0f2a7a63f3c51f4223dbd7afb487c93e5ff917
Vinicius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2023-03-05 16:40:32 -0300

    JsonIgnore ResidentialProperties

--- src/Api.Domain/Entities/Person.cs
+++ src/Api.Domain/Entities/Person.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Api.Domain.Entities
 {
     public class Person : BaseUser
     {
-        public virtual List<ResidentialProperty> ResidencialProperties { get; set; }
+        [JsonIgnore]
+        public virtual List<ResidentialProperty>? ResidentialProperties { get; set; }
     }
 }
