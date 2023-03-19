8a305e083303660842b31ae5eb7d0d843b47f112
Vinicius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2023-03-19 17:19:08 -0300

    Swagger Token Configuration

--- src/Api.Application/Program.cs
+++ src/Api.Application/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +20,47 @@ ConfigureRepository.ConfigureDependenciesRepository(builder.Services, mySqlConne
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Sistema SaneJá",
+        Version = "v1",
+        Description = "Uma API feita com ASP.NET Core",
+        Contact = new OpenApiContact
+        {
+            Name = "Vinícius Lemos de Oliveira",
+            Url = new Uri("https://linkedin.com/in/vinolemos"),
+            Email = "vlemosdeoliveira@gmail.com"
+        }
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Header de Autorização JWT usando o esquema BEARER." +
+        "\r\n\r\nInforme 'Bearer[espaço] e o seu token.\r\n\r\nExemplo: \'Bearer 12345abcdef\'",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<MyContext>()
@@ -41,11 +83,16 @@ builder.Services.AddAuthentication(
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.InjectStylesheet("/swagger-ui/SwaggerDark.css");
+    });
 }
 
 app.UseHttpsRedirection();
