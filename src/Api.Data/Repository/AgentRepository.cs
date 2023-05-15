43c188b16ecddf2ddff668a5d6bdb12c1b2a1472
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2023-05-15 20:50:59 -0300

    Agent Repository

--- src/Api.Data/Repository/AgentRepository.cs
+++ src/Api.Data/Repository/AgentRepository.cs
@@ -1,19 +1,19 @@
-﻿using Api.Data.Context;
+using Api.Data.Context;
 using Api.Data.Repository;
 using Api.Domain.Entities;
-using Api.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repository
 {
-    public class PersonRepository : IBaseRepository<User>
+    public class AgentRepository : IBaseRepository<User>
     {
         private readonly MyContext _context;
 
-        public PersonRepository(MyContext context)
+        public AgentRepository(MyContext context)
         {
             _context = context;
         }
+
         public Task<User> InsertAsync(User item)
         {
             throw new NotImplementedException();
@@ -24,7 +24,7 @@ namespace Data.Repository
             var user = await (from u in _context.Users
                               join ur in _context.UserRoles on u.Id equals ur.UserId
                               join r in _context.Roles on ur.RoleId equals r.Id
-                              where u.Id == id && r.Name == "Person"
+                              where u.Id == id && r.Name == "Agent"
                               select u).FirstOrDefaultAsync();
 
             return user ?? throw new ArgumentException("Usuário não encontrado");
@@ -35,7 +35,7 @@ namespace Data.Repository
             var users = await (from u in _context.Users
                                join ur in _context.UserRoles on u.Id equals ur.UserId
                                join r in _context.Roles on ur.RoleId equals r.Id
-                               where r.Name == "Person"
+                               where r.Name == "Agent"
                                select u).ToListAsync();
 
             return users;
@@ -45,22 +45,23 @@ namespace Data.Repository
             var user = await (from u in _context.Users
                               join ur in _context.UserRoles on u.Id equals ur.UserId
                               join r in _context.Roles on ur.RoleId equals r.Id
-                              where u.Id == id && r.Name == "Person"
+                              where u.Id == id && r.Name == "Agent"
                               select u).FirstOrDefaultAsync();
 
             return user != null;
         }
+
+
         public void UpdateAsync(User item)
         {
             throw new NotImplementedException();
         }
-
         public async Task<bool> DeleteAsync(Guid id)
         {
             var user = await (from u in _context.Users
                               join ur in _context.UserRoles on u.Id equals ur.UserId
                               join r in _context.Roles on ur.RoleId equals r.Id
-                              where u.Id == id && r.Name == "Person"
+                              where u.Id == id && r.Name == "Agent"
                               select u).FirstOrDefaultAsync();
 
             if (user == null) throw new ArgumentException("Usuário não encontrado");
